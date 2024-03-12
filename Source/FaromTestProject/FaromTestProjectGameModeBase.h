489b9f5d091d645482ddcf2484b609dc2f8cb0c7
ZloMishytka <misha.pilgui@ravenstalegamestudios.com>
2024-03-12 10:25:59 +0200

    Create ue4 project

---
+++ Source/FaromTestProject/FaromTestProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FaromTestProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FAROMTESTPROJECT_API AFaromTestProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
